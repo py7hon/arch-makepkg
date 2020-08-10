9891e6a52a6f5e2d1729f650198628a19ae261b4
Iqbal Rifai <yukifag@pm.me>
2020-08-10 18:22:40 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ echo "Running makepkg"
 
 # TODO: support extra flags
 
-su --login builder --command='makepkg -fs'
+su --login builder --command='makepkg -fs ./PKGBUILD'
 
 echo "Running namcap"
 
